2dece4017b740d0994a9a3166c0f7d85cc751d34
Denitsa2212 <denitsamarianova@gmail.com>
2023-05-18 01:27:21 +0300

    Commit for exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PhonebookList from './components/PhonebookList';
 import Input from './components/Input';
 import PersonsForm from './components/PersonsForm';
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+
+  // States
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filteredList, setFilteredList] = useState(persons);
+
+  // Effect
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setFilteredList(response.data)
+      })
+  }, [])
+
+  console.log('render', persons.length, 'people')
+  console.log(persons);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
