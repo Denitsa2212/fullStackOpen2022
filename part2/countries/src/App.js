5af62fa7f1947e514cbb427dfbb7d0ae1a261c41
Denitsa2212 <denitsamarianova@gmail.com>
2023-06-07 02:04:44 +0300

    Ex. 2.19

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -13,7 +13,7 @@ const SingularCountry = ({ info }) => {
       <p>Area: {info.area}</p>
       <p>Languages:</p>
       <ul>
-        {Object.values(info.languages).map((lang, ind) => <li key={ind}>{lang}</li>)}
+        {Object.values(info.languages).map((lang) => <li key={lang}>{lang}</li>)}
       </ul>
       <img src={info.flags.png} alt="flag" style={picStyle}/>
     </>
@@ -23,16 +23,14 @@ const SingularCountry = ({ info }) => {
 const App = () => {
   const [country, setCountry] = useState(null)
   const [showList, setShowList] = useState([])
+  const [countryToShow, setCountryToShow] = useState([])
   useEffect(() => {
-    console.log("in effect");
     if (country) {
-      // console.log("get countries");
       axios
         .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
         .then((res) => {
           const data = res.data;
           const resultList = data.filter(data => (data.name.common).toLowerCase().includes(country.toLowerCase()));
-          // setShowList(resultList.map(result => result.name.common))
           setShowList(resultList)
         })
         .catch(error => console.log(error))
@@ -42,15 +40,27 @@ const App = () => {
   const inputChange = (event) => {
     event.preventDefault();
     setCountry(event.target.value)
+    setCountryToShow([])
   }
-  
   return (
     <div>
         find countries: <input type="text" onChange={inputChange}/>
          
-        {showList.length === 1 ? <SingularCountry info={showList[0]}/> 
+          {countryToShow.length !== 0 ? 
+          (<SingularCountry info={countryToShow}/>) 
           : 
-        (showList.length > 10 ? (<p>Too many results</p>) : (showList.map((country, ind) => <p key={ind}>{country.name.common}</p>)))}
+          (showList.length === 1 ? <SingularCountry info={showList[0]}/> 
+            : 
+          (showList.length > 10 ? 
+            (<p>Too many results</p>) 
+            : 
+            (showList.map((country) => 
+            <>
+              <p>{country.name.common}</p>
+              <button onClick={() => setCountryToShow(country)}>show</button> 
+            </>))
+          ))
+          }
     </div>
   );
 };
